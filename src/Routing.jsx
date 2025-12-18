7428e41dc3eeacaaa37d2a01ec716624e7152e40
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2025-12-18 10:29:30 +0400

    Switch to HashRouter for GitHub Pages

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,21 +1,20 @@
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import App from "./App";
 import SignIn from "./SignIn";
 import Dashboard from "./dashboard";
 import SignUp from "./SignUp";
 
-function Routing(){
-
-return(
-<BrowserRouter basename="/My-Landing-Page">
-<Routes>
-    <Route path="/" element={<App/>} />
-    <Route path="/signIn" element={<SignIn/>} />
-    <Route path="/dashboard" element={<Dashboard/>} />
-    <Route path="/signUp" element={<SignUp/>} />
-</Routes>
-</BrowserRouter>
-)
-
+function Routing() {
+    return (
+        <HashRouter>
+            <Routes>
+                <Route path="/" element={<Navigate to="/signUp" />} />
+                <Route path="/signIn" element={<SignIn />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/signUp" element={<SignUp />} />
+            </Routes>
+        </HashRouter>
+    );
 }
-export default Routing
+
+export default Routing;
