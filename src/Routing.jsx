bd8d7df135b75070bed35824223937fe3bbd1183
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2025-12-18 11:12:06 +0400

    Add 404 redirect script to index.html

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import SignIn from "./SignIn";
 import Dashboard from "./dashboard";
@@ -6,14 +6,14 @@ import SignUp from "./SignUp";
 
 function Routing() {
     return (
-        <HashRouter>
+        <BrowserRouter basename="/My-Landing-Page">
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/signIn" element={<SignIn />} />
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/signUp" element={<SignUp />} />
             </Routes>
-        </HashRouter>
+        </BrowserRouter>
     );
 }
 
